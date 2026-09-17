8b65d68a5fd287429819030e2e6d998fe4ad566c
omgseoul <oneminuteseoul@gmail.com>
2026-09-18 00:00:15 +0900

    Update guesthouse-manager-app/app/src/main/java/com/oneminute/guesthousemanager/AttendanceActivity.java

--- guesthouse-manager-app/app/src/main/java/com/oneminute/guesthousemanager/AttendanceActivity.java
+++ guesthouse-manager-app/app/src/main/java/com/oneminute/guesthousemanager/AttendanceActivity.java
@@ -1,8 +1,10 @@
 package com.oneminute.guesthousemanager;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
@@ -13,7 +15,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class AttendanceActivity extends AppCompatActivity {
     private static final String LOGIN_URL = "https://omgseoul.github.io/oneminute/login.html";
+    private static final int FILE_CHOOSER_REQUEST = 2201;
     private WebView webView;
+    private ValueCallback<Uri[]> fileCallback;
 
     @Override
     protected void onCreate(Bundle state) {
@@ -39,8 +43,34 @@ public class AttendanceActivity extends AppCompatActivity {
                 return handleNavigation(Uri.parse(url));
             }
         });
-        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onShowFileChooser(WebView view, ValueCallback<Uri[]> callback, FileChooserParams params) {
+                if (fileCallback != null) fileCallback.onReceiveValue(null);
+                fileCallback = callback;
+                try {
+                    startActivityForResult(params.createIntent(), FILE_CHOOSER_REQUEST);
+                } catch (Exception error) {
+                    fileCallback = null;
+                    Toast.makeText(AttendanceActivity.this, "사진 선택기를 열지 못했습니다.", Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+                return true;
+            }
+        });
         webView.loadUrl(LOGIN_URL);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == FILE_CHOOSER_REQUEST && fileCallback != null) {
+            Uri[] result = resultCode == Activity.RESULT_OK
+                    ? WebChromeClient.FileChooserParams.parseResult(resultCode, data) : null;
+            fileCallback.onReceiveValue(result);
+            fileCallback = null;
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     private boolean handleNavigation(Uri uri) {
@@ -49,7 +79,7 @@ public class AttendanceActivity extends AppCompatActivity {
             if ("emergency".equals(action)) {
                 startActivity(new Intent(this, EmergencyReportActivity.class));
             } else if ("mission".equals(action)) {
-                Toast.makeText(this, "미션 기능은 다음 버전에 추가됩니다.", Toast.LENGTH_SHORT).show();
+                webView.loadUrl("https://omgseoul.github.io/oneminute/mission.html");
             } else if ("inbox".equals(action)) {
                 Toast.makeText(this, "사장님 확인함은 사장 계정에서 이용해주세요.", Toast.LENGTH_SHORT).show();
             }
